9159404022f2420f1af857965c81fa3089e28b1f
Alejandro Boidi Rico <alejandro.rico@gmail.com>
2022-10-05 23:00:33 -0300

    Cards feito

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -1,29 +1,15 @@
 import './home.scss';
 
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 
-import {
-  Row,
-  Col,
-  Alert,
-  Card,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  CardText,
-  Carousel,
-  CarouselItem,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselCaption,
-} from 'reactstrap';
+import { Row, Col, Card, CardBody, CardTitle, CardSubtitle, CardText, Fade, Container, CardColumns } from 'reactstrap';
 
 import { useAppSelector } from 'app/config/store';
 
 export const Home = () => {
   const account = useAppSelector(state => state.authentication.account);
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [isOpen] = useState(false);
+  const imgFolder = '../../../content/images/';
 
   return (
     <Row>
@@ -39,41 +25,90 @@ export const Home = () => {
         <p className="lead">Já pensou receber todos os dias</p>
         <h1>Mais cards aqui para produtos</h1>
       </Col>
-      <Col md="3"></Col>
-      <Col md="3">
-        <Card
-          style={{
-            width: '18rem',
-          }}
-        >
-          <img alt="Sample" src="../../../content/images/pao-caseiro.jpg" />
-          <CardBody>
-            <CardTitle tag="h5">Pão Fresco</CardTitle>
-            <CardSubtitle className="mb-2 text-muted" tag="h6">
-              2 vezes/semana
-            </CardSubtitle>
-            <CardText>Que tal receber em sua casa, na sua hora? :)</CardText>
-          </CardBody>
-        </Card>
+      <Row>
+        <Col md="3">
+          <Card
+            style={{
+              width: '18rem',
+            }}
+          >
+            <img alt="Sample" height={'169px'} src={imgFolder + 'bomba-chocolate.jpg'} />
+            <CardBody>
+              <CardTitle tag="h5">Bomba de Chocolate </CardTitle>
+              <CardSubtitle className="mb-2 text-muted" tag="h6">
+                4 vezes/semana
+              </CardSubtitle>
+              <CardText>Que tal um docinho?</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col md="3">
+          <Card
+            style={{
+              width: '18rem',
+            }}
+          >
+            <img alt="Sample" src={imgFolder + 'pao-caseiro.jpg'} />
+            <CardBody>
+              <CardTitle tag="h5">Pão Fresco</CardTitle>
+              <CardSubtitle className="mb-2 text-muted" tag="h6">
+                2 vezes/semana
+              </CardSubtitle>
+              <CardText>Que tal receber em sua casa, na sua hora? :)</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col md="3">
+          <Card
+            style={{
+              width: '18rem',
+            }}
+          >
+            <img alt="Sample" src={imgFolder + 'pao-forma-caseiro.jpg'} />
+            <CardBody>
+              <CardTitle tag="h5">Pão de Forma</CardTitle>
+              <CardSubtitle className="mb-2 text-muted" tag="h6">
+                Todos os dias
+              </CardSubtitle>
+              <CardText>Quanto pão!</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col md="3">
+          <Card
+            style={{
+              width: '18rem',
+            }}
+          >
+            <img alt="Sample" height={'169px'} src={imgFolder + 'rosquinhas.webp'} />
+            <CardBody>
+              <CardTitle tag="h5">Rosquinhas</CardTitle>
+              <CardSubtitle className="mb-2 text-muted" tag="h6">
+                Todos os dias
+              </CardSubtitle>
+              <CardText>Colorido!</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+      <Col lg="6">
+        <Row>
+          <h2>O que oferecemos?</h2>
+          <div>
+            Um plano de assinatura para sua padaria favorita! Imagina poder escolher uma cesta de qualquer produto que a padaria tem a
+            oferecer. Monte sua cesta, crie seu plano ou escolha a sua vontade e receba no momento que você quiser.
+          </div>
+          <Col md="4">
+            <div>Produtos de qualidade!</div>
+          </Col>
+          <Col md="4">
+            <div>Entrega na sua casa!</div>
+          </Col>
+          <Col md="4">
+            <div>Cesta personalizada!</div>
+          </Col>
+        </Row>
       </Col>
-      <Col md="6">
-        <Card
-          style={{
-            width: '18rem',
-          }}
-        >
-          <img alt="Sample" src="../../../content/images/pao-forma-caseiro.jpg" />
-          <CardBody>
-            <CardTitle tag="h5">Pão de Forma</CardTitle>
-            <CardSubtitle className="mb-2 text-muted" tag="h6">
-              Todos os dias
-            </CardSubtitle>
-            <CardText>Quanto pão!</CardText>
-          </CardBody>
-        </Card>
-      </Col>
-      <h1>Sobre nós, explicar o produto</h1>
-
       {/* {account?.login ? (
           <div>
             <Alert color="success">Você está logado como &quot;{account.login}&quot;.</Alert>
